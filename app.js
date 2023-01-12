5cf1b6e85bc62ee82f9dbb5af450301f8706d512
Michael Kestler <michael.s.kestler@gmail.com>
2023-01-11 22:41:47 -0800

    installed ejs. added views folder and list.ejs file.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+
+//use ejs as view engine
+app.set("view engine", "ejs");
+
 //for use in place of deprecated body-parser
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
@@ -9,7 +13,16 @@ app.use(express.json())
 const port = 3000;
 
 app.get("/", (req, res) =>{
-    res.send("Hello World!");
+    res.render('index', )
+    let today = new Date();
+    let currentDay = today.getDay();
+
+    if(currentDay === 6 || today.getDay() === 0) {
+        res.write("<h1>Yay it's the weekend</h1>");
+    } else {
+        res.sendFile(__dirname + "/index.html");
+    }
+    // res.send("Hello World!");
 });
 
 
