d9404ac713ad4306441f8c2b330bcf8a81badc14
Michael Kestler <michael.s.kestler@gmail.com>
2023-01-12 19:24:59 -0800

    added new route for work list todo list and created conditional which allows work list and the root to do list to use same view

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 let items = ["Buy Food", "Cook Dinner", "Eat Dinner"];
+let workItems = [];
 
 //use ejs as view engine
 app.set("view engine", "ejs");
@@ -13,6 +14,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const port = 3000;
+
+
+
 
 app.get("/", (req, res) => {
     let today = new Date();
@@ -28,15 +32,40 @@ app.get("/", (req, res) => {
 
     let day = today.toLocaleDateString("en-US", options)
 
-    res.render("list", {kindOfDay: day, newListItem: items});
+    res.render("list", {listTitle: day, newListItem: items});
 });
 
 app.post("/", (req, res) => {
     let item = req.body.newItem;
 
-    items.push(item);
-    res.redirect("/");
+    if(req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
 });
+
+
+
+app.get("/work", (req, res) =>{
+    res.render("list", {listTitle: "Work List", newListItem: workItems})
+})
+
+
+
+
+// app.post("/work",(req, res)=>{
+//     let item = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect("/work");
+//
+// })
+
+
+
+
 
 
 app.listen(port, ()=>{
